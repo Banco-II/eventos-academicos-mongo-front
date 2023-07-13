6680ceb431a7e24285164def09190ef9902760e4
franciscojrdev <franciscojunior7222@gmail.com>
2023-07-12 23:47:56 -0300

    feat: created signin and signup pages

---
+++ src/pages/SearchLocations/index.js
@@ -0,0 +1,45 @@
+// import { useState, useEffect } from "react";
+// import axios from "axios";
+
+// export default function SearchLocation() {
+//   const [busca, setBusca] = useState({ conteudo: "" });
+//   const [data, setData] = useState();
+
+//   function handleBusca(e) {
+//     setBusca({ ...busca, [e.target.name]: e.target.value });
+//   }
+
+//   useEffect(() => {
+//     const url = `http://localhost:4000/event/${busca.conteudo}`;
+
+//     axios
+//       .get(url)
+//       .then((res) => {
+//         console.log(res.data);
+//         setData(res.data);
+//       })
+//       .catch((err) => {
+//         console.log("Ocorreu um erro: " + err);
+//       });
+//   });
+
+//   return (
+//     <>
+//       <div className="container">
+//         <h1>Buscar Evento</h1>
+
+//         <form>
+//           <input
+//             type="text"
+//             placeholder="Buscar evento"
+//             className="busca"
+//             name="conteudo"
+//             required
+//             value={busca.conteudo}
+//             onChange={handleBusca}
+//           />
+//         </form>
+//       </div>
+//     </>
+//   );
+// }
