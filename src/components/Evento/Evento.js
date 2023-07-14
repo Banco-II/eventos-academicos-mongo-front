bc13b1f6172b258774c31c9bea8f80034d10dfa9
franciscojrdev <franciscojunior7222@gmail.com>
2023-07-13 23:03:28 -0300

    creating search

--- src/components/Evento/Evento.js
+++ src/components/Evento/Evento.js
@@ -1,19 +1,17 @@
 const Evento = ({ evento }) => {
-
   return (
-  <div className="evento">
-  <div className="content">
-  <p>{evento.titulo}</p>
-  <p className="descricao">{evento.descricao}</p>
-  <p className="dataInicio">{evento.dataInicio}</p>
-  <p className="dataTermino">{evento.dataTermino}</p>
-  </div>
-  <div>
-  <button className='participar'>Participar</button>
-  </div>
-  </div>
-  )
-  }
-  
-  export default Evento
-  
+    <div className="evento">
+      <div className="content">
+        <p>Titulo: {evento.titulo}</p>
+        <p className="descricao">Descrição: {evento.descricao}</p>
+        <p className="dataInicio">Data de Inicio: {evento.dataInicio.slice(0,10)}</p>
+        <p className="dataTermino">Data de Término: {evento.dataTermino.slice(0,10)}</p>
+      </div>
+      <div>
+        <button className="participar">Participar</button>
+      </div>
+    </div>
+  );
+};
+
+export default Evento;
